45ae1081ee198cf5639c59c4f6b64422d8e943d4
NetRunner <galatskiy@gmail.com>
2023-08-06 12:44:32 +0300

    Initial Commit

---
+++ PlantTheSeed/PlantTheSeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantTheSeedApp.swift
+//  PlantTheSeed
+//
+//  Created by Dmitrii Galatskii on 06.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct PlantTheSeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
