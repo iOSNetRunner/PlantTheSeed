688e4af59b61ef8abeca527f8b288b7c8f8a19a9
NetRunner <galatskiy@gmail.com>
2023-08-07 00:51:15 +0300

    Plant redraw.

--- PlantTheSeed/ContentView.swift
+++ PlantTheSeed/ContentView.swift
@@ -14,6 +14,8 @@ struct ContentView: View {
     @State private var start = false
     @State private var startTwo = false
     @State private var startThree = false
+    @State private var startFour = false
+    @State private var startFive = false
     @State private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     
@@ -29,9 +31,27 @@ struct ContentView: View {
                     
                     PlantSkeletonView()
                         .trim(from: 0.0, to: pathProgress)
-                        .fill(Gradient(colors: [.red, .green, .black]))
+                        .fill(Gradient(colors: [.green, .black]))
                         .shadow(color: .green, radius: 3)
-                        .shadow(color: .yellow, radius: 1)
+                        .shadow(color: .black, radius: 1)
+                        .scaleEffect(startFive ? 0.9 : 0.95)
+                        .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
+                        .offset(x: -30, y: startFive ? -75 : -80)
+                        .rotationEffect(.degrees(start ? -20 : -10))
+                        .onAppear() {
+                            withAnimation(
+                                .easeInOut(duration: 2).repeatForever().delay(2)
+                            ) {
+                                startFive = true
+                                
+                            }
+                        }
+                    
+                    PlantSkeletonView()
+                        .trim(from: 0.0, to: pathProgress)
+                        .fill(Gradient(colors: [.green, .black]))
+                        .shadow(color: .green, radius: 3)
+                        .shadow(color: .black, radius: 1)
                         .scaleEffect(start ? 1 : 1.1)
                         .offset(x: start ? 5 : 0, y: start ? 0 : -40)
                         .rotationEffect(.degrees(start ? 1 : -2))
@@ -47,11 +67,12 @@ struct ContentView: View {
                     
                     PlantSkeletonView()
                         .trim(from: 0.0, to: pathProgress)
-                        .fill(Gradient(colors: [.red, .green, .black]))
-                        .rotationEffect(.degrees(startTwo ? 30 : 20), anchor: .bottom)
-                        .offset(x: -50, y: 68)
+                        .fill(Gradient(colors: [.black, .green, .black]))
+                        .rotationEffect(.degrees(startTwo ? -30 : -25), anchor: .bottom)
+                        .offset(x: -30, y: 68)
+                        .rotation3DEffect(.degrees(180), axis: (x: 0, y: 1, z: 0))
                         .shadow(color: .green, radius: 5)
-                        .shadow(color: .yellow, radius: 1)
+                        .shadow(color: .black, radius: 1)
                         .scaleEffect(0.7)
                         .onAppear() {
                             withAnimation(
@@ -63,11 +84,27 @@ struct ContentView: View {
                     
                     PlantSkeletonView()
                         .trim(from: 0.0, to: pathProgress)
-                        .fill(Gradient(colors: [.red, .green, .black]))
-                        .rotationEffect(.degrees( startThree ? -45 : -30), anchor: .bottom)
+                        .fill(Gradient(colors: [.black, .green, .black]))
+                        .rotationEffect(.degrees(startFour ? 7 : 7), anchor: .bottom)
+                        .offset(x: 0, y: 160)
+                        .shadow(color: .green, radius: 5)
+                        .shadow(color: .black, radius: 1)
+                        .scaleEffect(startFour ? 0.8: 0.75)
+                        .onAppear() {
+                            withAnimation(
+                                .easeInOut(duration: 5).repeatForever().delay(1)
+                            ) {
+                                startFour.toggle()
+                            }
+                        }
+                    
+                    PlantSkeletonView()
+                        .trim(from: 0.0, to: pathProgress)
+                        .fill(Gradient(colors: [.black, .green, .black]))
+                        .rotationEffect(.degrees( startThree ? -45 : -40), anchor: .bottom)
                         .offset(x: 80, y: 100)
                         .shadow(color: .green, radius: 5)
-                        .shadow(color: .yellow, radius: 1)
+                        .shadow(color: .black, radius: 2)
                         .scaleEffect(0.6)
                         .onAppear() {
                             withAnimation(
@@ -80,7 +117,7 @@ struct ContentView: View {
                     Circle()
                         .trim(from: 0, to: 0.5)
                         
-                        .fill(RadialGradient(colors: [.yellow, .black], center: .center, startRadius: 1, endRadius: 90))
+                        .fill(RadialGradient(colors: [.white, .black], center: .center, startRadius: 1, endRadius: 110))
                         
                         
                         
@@ -92,7 +129,7 @@ struct ContentView: View {
                         .blendMode(.plusLighter)
                         .scaledToFit()
                         .position(x: 200, y: 480)
-                        .shadow(color: .black, radius: 3)
+                        .shadow(color: .black, radius: 5)
                         
                         
 
